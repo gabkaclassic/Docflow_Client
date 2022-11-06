9a2830a7c9b82858a704d9f8065acbcbedb56109
Errodion <kuzminrd44@gmail.com>
2022-11-06 19:12:38 +0300

    Bug with null objects was fixed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module client.gui {
+    
     requires javafx.controls;
     requires javafx.fxml;
-    
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
@@ -17,7 +17,6 @@ module client.gui {
     requires reactor.core;
     requires java.datatransfer;
     
-    opens client.gui to javafx.fxml;
     exports client.response to com.fasterxml.jackson.databind;
     exports client.entity.process to com.fasterxml.jackson.databind;
     exports client.entity.user to com.fasterxml.jackson.databind;
@@ -27,6 +26,8 @@ module client.gui {
     exports client.gui;
     exports client.response.deserializer to com.fasterxml.jackson.databind;
     exports client.gui.controller;
+    
+    opens client.gui to javafx.fxml;
     opens client.gui.controller to javafx.fxml;
     
 }
